5c72bc7d8fec068a272181d797d0004ff7adaa17
blue-sky-r <robert.puskajler@yahoo.ca>
2022-05-06 11:08:28 +0200

    mrtg probe - He rewards - small improvements

--- mrtg/mrtg-he-rewards.sh
+++ mrtg/mrtg-he-rewards.sh
@@ -1,46 +1,76 @@
 #!/bin/bash
 
-# usage help
+# FOR: CONTROLLINO miner only
+#  firmware_version: raspbian bionic 2022.03.23.1 + dashboard 1.2.1 - 1.3.4
+#  firmware_version: raspbian bionic 2022.04.19.1 + dashboard 1.3.5
+
+# about
 #
-[ "$1" == "-h" ] && cat <<< """
-= mrtg probe to graph He rewards =
+_about_="mrtg probe to graph He miner rewards"
 
-usage: $0 [-h] [-d] [host [rewards]]
-
--h     ... show this usage help
--d     ... additional debug info
-host   ... host to connect to (default controllinohotspot)
-reward ... reward interval to query (default 1day = 24h)
-""" && exit 1
-
-# debug
+# version
 #
-[ "$1" == "-d" ] && DBG=1 && shift
+_version_="2022.05.06"
 
-# He miner hostname
+# github
 #
-host=${1:-controllinohotspot}
+_github_="https://github.com/blue-sky-r/controllino-he-tools/blob/main/mrtg/mrtg-he-rewards.sh"
 
-# time range for rewards: 1/7/14 days
+# DEFAULT He miner hostname
 #
-rewards=${2:-1}
+host="controllinohotspot"
 
-# json message keys to display in this order
+# DEFAULT time range for rewards: 1/7/14 days
 #
-keys="height,sync height,uptime,miner name"
+days=1
+
+# json message keys and multipliers to display in this order, format ("key",mult),("key2",mult2)...
+#
+keys='("sum",1),("avg",1E9)'
 
 # wget options
 #
 opts="--retry-on-host-error --retry-connrefused --retry-on-http-error=503 --timeout=7 --tries=3 -q -O -"
 
+# python interpreter
+#
+python="python3"
+
+# usage help
+#
+[ "$1" == "-h" ] && cat <<< """
+= $_about_ = ver $_version_ = $_github_ =
+
+usage: $0 [-h] [-d] [host [days]]
+
+-h   ... show this usage help
+-d   ... additional debug info
+host ... host/miner to connect to (default $host)
+days ... reward interval to query in days, valid values are 1,7,14 (default $days)
+""" && exit 1
+
+# optional debug
+#
+[ "$1" == "-d" ] && DBG=1 && shift
+
+# o[tional He miner hostname
+#
+[ $1 ] && host=$1
+
+# optional rewards period
+#
+[ $2 ] && days=$2
+
 # url to connect to
 #
-url=http://$host/rewards/$rewards
+url=http://$host/rewards/$days
 
 # json response
 #
 json=$( wget $opts $url )
 
+# debug output
+#
 [ $DBG ] && echo "DBG.URL: $url" && echo "DBG.JSON: $json" && echo
 
 # {"status":200,"rewards":{"total":0.31404812,"sum":31404812,"stddev":0.012765751345,"min":0,"median":0.01125426,"max":0,"avg":0.0125619248}}
@@ -50,16 +80,18 @@ json=$( wget $opts $url )
 pycode=$( cat <<___
 import sys,json
 try:
-  j = json.loads(r'$json')
+    j = json.loads(r'$json')
 except json.decoder.JSONDecodeError:
-  sys.exit(-1)
+    sys.exit(-1)
 if j.get('status') != 200: sys.exit(-1)
 r = j.get('rewards')
-print(r.get('sum'))
-print(round(r.get('avg') * 10E8))
+for key,mult in ($keys):
+    print(round(r.get(key,0) * mult))
 print('?')
 print('$host')
 ___
 )
 
-python3 -c "$pycode"
+# execute
+#
+$python -c "$pycode"
