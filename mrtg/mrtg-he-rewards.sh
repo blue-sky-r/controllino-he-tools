9e6ef9b93a936715fa91bfd3a26af6390431e3d0
blue-sky-r <robert.puskajler@yahoo.ca>
2022-05-11 19:28:39 +0200

    mrtg probe - refactored to support any expression in key via eval()

--- mrtg/mrtg-he-rewards.sh
+++ mrtg/mrtg-he-rewards.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# FOR: CONTROLLINO miner only
+# For CONTROLLINO miner only - https://hotspot.controllino.com/
 #  firmware_version: raspbian bionic 2022.03.23.1 + dashboard 1.2.1 - 1.3.4
-#  firmware_version: raspbian bionic 2022.04.19.1 + dashboard 1.3.5
+#  firmware_version: raspbian bionic 2022.04.27.0 + dashboard 1.3.5
 
 # about
 #
@@ -10,7 +10,7 @@ _about_="mrtg probe to graph He miner rewards"
 
 # version
 #
-_version_="2022.05.06"
+_version_="2022.05.11"
 
 # github
 #
@@ -24,9 +24,9 @@ host="controllinohotspot"
 #
 days=1
 
-# json message keys and multipliers to display in this order, format ("key",mult),("key2",mult2)...
+# json response keys to expressions to eveluate to display in this order
 #
-keys='("sum",1),("avg",1E9)'
+keys="sum, avg * 1E9"
 
 # wget options
 #
@@ -39,7 +39,7 @@ python="python3"
 # usage help
 #
 [ "$1" == "-h" ] && cat <<< """
-= $_about_ = ver $_version_ = $_github_ =
+= $_about_ = ver $_version_ =
 
 usage: $0 [-h] [-d] [host [days]]
 
@@ -47,13 +47,15 @@ usage: $0 [-h] [-d] [host [days]]
 -d   ... additional debug info
 host ... host/miner to connect to (default $host)
 days ... reward interval to query in days, valid values are 1,7,14 (default $days)
+
+$_github_
 """ && exit 1
 
 # optional debug
 #
 [ "$1" == "-d" ] && DBG=1 && shift
 
-# o[tional He miner hostname
+# optional He miner hostname
 #
 [ $1 ] && host=$1
 
@@ -65,29 +67,30 @@ days ... reward interval to query in days, valid values are 1,7,14 (default $day
 #
 url=http://$host/rewards/$days
 
-# json response
-#
-json=$( wget $opts $url )
+[ $DBG ] && echo "DBG.URL: $url"
 
-# debug output
+# get json response
 #
-[ $DBG ] && echo "DBG.URL: $url" && echo "DBG.JSON: $json" && echo
+json=$( wget $opts $url ); excode=$?
 
 # {"status":200,"rewards":{"total":0.31404812,"sum":31404812,"stddev":0.012765751345,"min":0,"median":0.01125426,"max":0,"avg":0.0125619248}}
+[ $DBG ] && echo "DBG.WGET.exitcode: $excode" && echo "DBG.JSON: $json" && echo "DBG.KEYS: $keys" && echo
 
 # python code to process json
 #
 pycode=$( cat <<___
 import sys,json
-try:
-    j = json.loads(r'$json')
-except json.decoder.JSONDecodeError:
-    sys.exit(-1)
-if j.get('status') != 200: sys.exit(-1)
-r = j.get('rewards')
-for key,mult in ($keys):
-    print(round(r.get(key,0) * mult))
+try: j = json.loads(r'$json')
+except json.decoder.JSONDecodeError: sys.exit(-1)
+if j.get('status') != 200: sys.exit(-2)
+vars = j.get('rewards')
+for key in '$keys'.split(', '):
+    if '$DBG': print('DBG.key:', key, end=' => ')
+    v = eval(key, vars)
+    print(round(v))
+if '$DBG': print('DBG.UPTIME:', end=' ')
 print('?')
+if '$DBG': print('DBG.HOST:', end=' ')
 print('$host')
 ___
 )
