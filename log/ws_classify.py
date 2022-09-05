abd445540e90fca5bec6a7c2a2aeee03b8368309
blue-sky-r <robert.puskajler@yahoo.ca>
2022-09-05 09:09:12 +0200

    refactored config dict, table float columns handling, signals

--- log/ws_classify.py
+++ log/ws_classify.py
@@ -14,12 +14,76 @@ import re
 import signal
 import time
 
-__VERSION__ = '2022.09.03'
+__VERSION__ = '2022.09.05'
 
 
 # debog cmponents (csv)
 #
 DBGMODE = ''
+
+# miner specific config
+#
+CONFIG = {
+    # config for firmware version
+    'miner_version': '2022.08.17.1',
+    # match console log line parts
+    # 2022-08-28 10:42:39.904 7 [error] <0.3591.0>@Undefined:Undefined:Undefined gen_server <0.3591.0> terminated with reason: connection_down
+    # 2022-08-20 10:26:34.913 7 [info] <0.1746.0>@miner_lora_light:handle_udp_packet:{350,5} PULL_DATA from 12273815315514654720 on 57675
+    'log': re.compile(r'^(?P<datetime>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}) \d+ '
+                       '\[(?P<level>\w+)\] '
+                       '<[\d.]+>@(?P<facility>\w+:\w+(:[\w ]+)?):?[<{](?P<loc>[\d,.]+)[}>] '
+                       '(?P<msg>.+)$'),
+    # signal -> action name
+    'signals': {
+        signal.SIGUSR1: 'onsignal_usr1'
+    },
+    # classify sections
+    'classify': {
+        'level': {
+            'desc': 'level count',
+            'facility': None,
+            'match': None,
+            'onmatch': 'level_count_onmatch',
+        },
+        'facility': {
+            'desc': 'facility count',
+            'facility': None,
+            'match': None,
+            'onmatch': 'facility_count_onmatch',
+        },
+        'witness': {
+            'desc': 'witnessing count and table',
+            # 2022-07-20 16:07:19.027 8 [info] <0.1778.0>@miner_onion_server_light:decrypt:{230,13} sending witness at RSSI: -139, Frequency: 867.1, SNR: -19.5
+            'facility': 'miner_onion_server_light:decrypt',
+            'match': re.compile(r'sending witness at RSSI: (?P<rssi>-\d+), Frequency: (?P<freq>\d+\.\d+), SNR: (?P<snr>-?\d+\.?\d*)'),
+            'onmatch': 'witnessing_onmatch',
+            'table': {
+                'header': 'Date Time GMT, Fre, RSSI, SNR',
+                # float columns f will have average values calculated in the tab footer
+                'format': '| {0:>23s} | {1:>6.2f} | {2:>6.1f} | {3:>5.1f} |',
+                'footer': 'average values, x',
+                'maxrows': 10
+            }
+        },
+        'uplink': {
+            'desc': 'uplink received',
+            # 2022-08-17 11:07:26.227 7 [info] <0.1647.0>@miner_mux_port:dispatch_port_logs:{118,13} [ gwmp-mux ] From AA:55:5A:00:00:00:00:00 received uplink: @3383561764 us, 868.10 MHz, DataRate(SF12, BW125), rssis: -142, snr: -22.2, len: 22
+            'facility': 'miner_mux_port:dispatch_port_logs',
+            'match': re.compile(
+                r'\[ gwmp-mux \] From AA:55:5A:00:00:00:00:00 received uplink: @\d+ us, (?P<freq>\d+\.\d+) MHz, DataRate\((?P<sf>SF\d+), (?P<bw>BW\d+)\), rssis: (?P<rssi>-\d+), snr: (?P<snr>-?\d+\.?\d*), len: (?P<len>\d+)'
+            ),
+            'onmatch': 'uplink_onmatch',
+            'table': {
+                'header': 'Date Time GMT, Freq, SF, BW, RSSI, SNR, Len',
+                # float columns f will have average values calculated in the tab footer
+                'format': '| {0:>23s} | {1:>6.2f} | {2:<4s} {3:>5s} | {4:>6.1f} | {5:>5.1f} | {6:>5.1f} |',
+                'footer': 'average values, x',
+                'maxrows': 15
+            }
+        }
+    }
+}
+
 
 def dbg(component, msg):
     """ print dbg msg if requested in global DBGMODE """
@@ -98,9 +162,9 @@ class Classifier:
             table = entry.get('table')
             if table:
                 self.tab[section] = Table(table['header'], table['format'], table['footer'], table.get('maxrows'))
-        # attach signal handlers - doesn't work here
-        #for sig, fnc in CONFIG.get('signals').items():
-        #    signal.signal(sig, fnc)
+        # attach signal handlers
+        for sig, fncname in CONFIG.get('signals').items():
+            signal.signal(sig, self.__getattribute__(fncname))
 
     def stat_count(self, cntname, match, key=None):
         """ cnt[cntname][match[key]]++ """
@@ -122,8 +186,8 @@ class Classifier:
         self.stat_count('witness', linematch, 'sending witness')
         self.tab['witness'].add_row((
             linematch['datetime'],
-            msgmatch['freq'],
-            msgmatch['rssi'],
+            float(msgmatch['freq']),
+            float(msgmatch['rssi']),
             float(msgmatch['snr'])
         ))
 
@@ -132,14 +196,13 @@ class Classifier:
         self.stat_count('uplink', linematch, 'received uplink')
         self.tab['uplink'].add_row((
             linematch['datetime'],
-            msgmatch['freq'],
+            float(msgmatch['freq']),
             msgmatch['sf'],
             msgmatch['bw'],
-            msgmatch['rssi'],
+            float(msgmatch['rssi']),
             # add decimal .0 for integer values for alignment
-            #msgmatch['snr'] if '.' in msgmatch['snr'] else msgmatch['snr']+'.0',
             float(msgmatch['snr']),
-            msgmatch['len']
+            float(msgmatch['len'])
         ))
 
     def classify(self, line):
@@ -162,7 +225,7 @@ class Classifier:
                 if msgmatch is None: continue
             # call onmatch fnc if defined
             if entry.get('onmatch'):
-                entry['onmatch'](self, line, linematch, msgmatch)
+                self.__getattribute__(entry.get('onmatch'))(line, linematch, msgmatch)
 
     def output_stats(self, file=sys.stdout):
         """ counters and tabs to file """
@@ -183,75 +246,12 @@ class Classifier:
             self.output_stats(file=file)
 
 
-# miner specific config
-#
-CONFIG = {
-    # config for firmware version
-    'miner_version': '2022.08.17.1',
-    # match log line parts
-    # 2022-08-28 10:42:39.904 7 [error] <0.3591.0>@Undefined:Undefined:Undefined gen_server <0.3591.0> terminated with reason: connection_down
-    # 2022-08-20 10:26:34.913 7 [info] <0.1746.0>@miner_lora_light:handle_udp_packet:{350,5} PULL_DATA from 12273815315514654720 on 57675
-    'log': re.compile(r'^(?P<datetime>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}) \d+ '
-                       '\[(?P<level>\w+)\] '
-                       '<[\d.]+>@(?P<facility>\w+:\w+(:[\w ]+)?):?[<{](?P<loc>[\d,.]+)[}>] '
-                       '(?P<msg>.+)$'),
-    # signal action
-    #'signals': {
-    #    signal.SIGUSR1: Classifier.onsignal_usr1
-    #},
-    # classify sections
-    'classify': {
-        'level': {
-            'desc': 'level count',
-            'facility': None,
-            'match': None,
-            'onmatch': Classifier.level_count_onmatch,
-        },
-        'facility': {
-            'desc': 'facility count',
-            'facility': None,
-            'match': None,
-            'onmatch': Classifier.facility_count_onmatch,
-        },
-        'witness': {
-            'desc': 'witnessing count and table',
-            # 2022-07-20 16:07:19.027 8 [info] <0.1778.0>@miner_onion_server_light:decrypt:{230,13} sending witness at RSSI: -139, Frequency: 867.1, SNR: -19.5
-            'facility': 'miner_onion_server_light:decrypt',
-            'match': re.compile(r'sending witness at RSSI: (?P<rssi>-\d+), Frequency: (?P<freq>\d+\.\d+), SNR: (?P<snr>-?\d+\.?\d*)'),
-            'onmatch': Classifier.witnessing_onmatch,
-            'table': {
-                'header': 'Date Time GMT, Fre, RSSI, SNR',
-                'format': '| {0:>23s} | {1:>5s} | {2:>4s} | {3:>5.1f} |',
-                'footer': 'average values, x',
-                'maxrows': 10
-            }
-        },
-        'uplink': {
-            'desc': 'uplink received',
-            # 2022-08-17 11:07:26.227 7 [info] <0.1647.0>@miner_mux_port:dispatch_port_logs:{118,13} [ gwmp-mux ] From AA:55:5A:00:00:00:00:00 received uplink: @3383561764 us, 868.10 MHz, DataRate(SF12, BW125), rssis: -142, snr: -22.2, len: 22
-            'facility': 'miner_mux_port:dispatch_port_logs',
-            'match': re.compile(
-                r'\[ gwmp-mux \] From AA:55:5A:00:00:00:00:00 received uplink: @\d+ us, (?P<freq>\d+\.\d+) MHz, DataRate\((?P<sf>SF\d+), (?P<bw>BW\d+)\), rssis: (?P<rssi>-\d+), snr: (?P<snr>-?\d+\.?\d*), len: (?P<len>\d+)'
-            ),
-            'onmatch': Classifier.uplink_onmatch,
-            'table': {
-                'header': 'Date Time GMT, Freq, SF, BW, RSSI, SNR, Len',
-                'format': '| {0:>23s} | {1:>6s} | {2:<4s} {3:>5s} | {4:>4s} | {5:>5.1f} | {6:>3s} |',
-                'footer': 'average values, x',
-                'maxrows': 10
-            }
-        }
-    }
-}
-
-
 if __name__ == "__main__":
-    # cli pars
-    parser = argparse.ArgumentParser(description='classify controllino miner log reading from stdin',
+    # cli pars - https://docs.python.org/3/howto/argparse.html
+    parser = argparse.ArgumentParser(description='classify controllino miner console log reading from stdin',
                                      epilog='example: ws_tail.py -f con controllino | ws_classify.py')
-    #parser.add_argument('wserver',  metavar='[ws[s]://]host', help='websocket server to connect to')
-    #parser.add_argument('-q', '--quiet',  default='', required=False,
-    #                    help='quiet mode, do not passthrough processed line (default to stdout)')
+    #parser.add_argument('-q', '--quiet', default=False, required=False, action="store_true",
+    #                    help='quiet mode, do not passthrough processed line (default all to stdout)')
     #parser.add_argument('-c', '--classify', default='', required=False,
     #                    help='use internal classifier')
     parser.add_argument('-d', '--debug', metavar='c1[,c2]', default='', required=False,
@@ -261,7 +261,6 @@ if __name__ == "__main__":
 
     #
     c = Classifier(CONFIG)
-    signal.signal(signal.SIGUSR1, c.onsignal_usr1)
     #
     #with open('console.log', 'r') as f:
     #    for line in f:
