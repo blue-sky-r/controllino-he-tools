bca953b0390cfa0824e113b346aa6fbb8a443e3c
blue-sky-r <robert.puskajler@yahoo.ca>
2023-08-14 19:49:15 +0200

    mrtg monitor for witnessing - added local cache

--- mrtg/mrtg-he-witness-sel-unsel.sh
+++ mrtg/mrtg-he-witness-sel-unsel.sh
@@ -13,7 +13,7 @@ _about_="He selected and unselected witnessing"
 
 # version
 #
-_version_="2023.08.13"
+_version_="2023.08.14"
 
 # github
 #
@@ -37,9 +37,17 @@ activities=Witness
 key1=Selected
 key2=Unselected
 
+# local cache
+#
+cache_dir=/tmp
+
+# local cache lifetime in minutes
+#
+#cache_ttl_mins=87
+
 # wget options
 #
-opts="--retry-on-host-error --retry-connrefused --retry-on-http-error=503 --timeout=7 --tries=3 -q -O -"
+opts="--retry-on-host-error --retry-connrefused --retry-on-http-error=503 --timeout=7 --tries=3 -q -N"
 
 # usage help
 #
@@ -72,17 +80,36 @@ $_github_
 #
 [ $DBG ] && echo "DBG.activities: $activities" && echo "DBG.range: $range"
 
+# cache timestamp
+#
+#cache_timestamp=0; [ -s "$cache_html" ] && cache_timestamp=$(date -r "$cache_html" +%s)
+#
+#cache_expiry=$(date -d "now - $cache_ttl_mins mins" +%s)
+#
+#[ $DBG ] && echo "DBG.cache_html: $cache_html cache_timestamp: $cache_timestamp cache_expiry: $cache_expiry"
+
 # explorer URL
 #
-url="https://explorer.moken.io/hotspots/$hid/activity?activityRange=$range&selectedActivities=$activities"
+#url="https://explorer.moken.io/hotspots/$hid/activity?activityRange=$range&selectedActivities=$activities&selectedLayer=hotspots"
+#url="https://explorer.moken.io/hotspots/$hid/activity?activityRange=$range&selectedActivities=$activities"
+page="activity?activityRange=$range&selectedActivities=$activities"
+url="https://explorer.moken.io/hotspots/$hid/$page"
 #
 [ $DBG ] && echo "DBG.URL: $url"
 
+# conditional cache update
+#
+#[ ! -f "$cache_html" -o ! -s "$cache_html" -o $cache_timestamp -lt $cache_expiry ] && echo "DBB updating cache" && wget $opts -O "$cache_html" "$url"
+wget $opts -P "$cache_dir" "$url"; exitcode=$?
+
+#[ $DBG ] && echo "DBG.wget-exitcode: $exitcode"
+[ $DBG ] && ls -l "$cache_dir/$page"
+
 # get selected and unselected witnessing
+# wget -O - "https://explorer.moken.io/hotspots/112A7iiPUF6cKCRGE4CT646LkQJ5gUmU51HsFYqtW5914fN9pMub/activity?activityRange=8H&selectedActivities=Witness" | grep -o "\(Uns\|S\)elected"
+witnessing=$( grep -o '\('$key1'\)\|\('$key2'\)' "$cache_dir/$page" )
 #
-witnessing=$( wget $opts "$url" | grep -o '\('$key1'\)\|\('$key2'\)' ); excode=$?
-#
-[ $DBG ] && echo "DBG.WGET.exitcode: $excode" && echo "DBG.witnessing: "$witnessing && echo
+[ $DBG ] && echo "DBG.witnessing: "$witnessing && echo
 
 # Selected
 [ $DBG ] && echo -n "DBG.$key1: "
