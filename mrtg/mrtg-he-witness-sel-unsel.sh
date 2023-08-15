8ffd2602af2dd5fed2b3cdaf01c3fd99742bc99c
blue-sky-r <robert.puskajler@yahoo.ca>
2023-08-15 17:37:02 +0200

    mrtg monitor for witnessing - added local cache with expiry time

--- mrtg/mrtg-he-witness-sel-unsel.sh
+++ mrtg/mrtg-he-witness-sel-unsel.sh
@@ -13,7 +13,7 @@ _about_="He selected and unselected witnessing"
 
 # version
 #
-_version_="2023.08.14"
+_version_="2023.08.15"
 
 # github
 #
@@ -37,17 +37,17 @@ activities=Witness
 key1=Selected
 key2=Unselected
 
-# local cache
+# local cache dir
 #
-cache_dir=/tmp
+cachedir=/tmp
 
-# local cache lifetime in minutes
+# local cache lifetime
 #
-#cache_ttl_mins=87
+cachettl='65 mins'
 
 # wget options
 #
-opts="--retry-on-host-error --retry-connrefused --retry-on-http-error=503 --timeout=7 --tries=3 -q -N"
+opts="--retry-on-host-error --retry-connrefused --retry-on-http-error=503 --timeout=7 --tries=3 -N"
 
 # usage help
 #
@@ -78,36 +78,41 @@ $_github_
 
 # use activities and range
 #
-[ $DBG ] && echo "DBG.activities: $activities" && echo "DBG.range: $range"
-
-# cache timestamp
-#
-#cache_timestamp=0; [ -s "$cache_html" ] && cache_timestamp=$(date -r "$cache_html" +%s)
-#
-#cache_expiry=$(date -d "now - $cache_ttl_mins mins" +%s)
-#
-#[ $DBG ] && echo "DBG.cache_html: $cache_html cache_timestamp: $cache_timestamp cache_expiry: $cache_expiry"
+[ $DBG ] && echo "DBG.explorer activities: $activities range: $range"
 
 # explorer URL
 #
-#url="https://explorer.moken.io/hotspots/$hid/activity?activityRange=$range&selectedActivities=$activities&selectedLayer=hotspots"
+#url="https://explorer.moken.io/hotspots/$hid/activity?activityRange=$range&selectedActivities=$activities&selectedLayer=hotspot"
 #url="https://explorer.moken.io/hotspots/$hid/activity?activityRange=$range&selectedActivities=$activities"
 page="activity?activityRange=$range&selectedActivities=$activities"
 url="https://explorer.moken.io/hotspots/$hid/$page"
 #
 [ $DBG ] && echo "DBG.URL: $url"
 
-# conditional cache update
+# cache timestamp
 #
-#[ ! -f "$cache_html" -o ! -s "$cache_html" -o $cache_timestamp -lt $cache_expiry ] && echo "DBB updating cache" && wget $opts -O "$cache_html" "$url"
-wget $opts -P "$cache_dir" "$url"; exitcode=$?
+cachets=0; [ -s "$cachedir/$page" ] && cachets=$(date -r "$cachedir/$page" +%s)
 
-#[ $DBG ] && echo "DBG.wget-exitcode: $exitcode"
-[ $DBG ] && ls -l "$cache_dir/$page"
+# cache expiration timestamp
+#
+expts=$(date -d "now - $cachettl" +%s)
+#
+[ $DBG ] && echo "DBG.CACHE file: $cachedir/$page  has timestamp: $cachets expiry $cachettl is timestamp: $expts or lower"
+
+# cache empty or expired - update cache
+#
+if [ ! -f "$cachedir/$page" -o ! -s "$cachedir/$page" -o $cachets -lt $expts ]
+then
+	[   $DBG ] && echo "DBB.wget updating cache"
+	[ ! $DBG ] && opts="$opts -q"
+	wget $opts -P "$cachedir" "$url"
+fi
+#
+[ $DBG ] && echo "DBG.CACHE: "$(ls -l $cachedir/$page)
 
 # get selected and unselected witnessing
 # wget -O - "https://explorer.moken.io/hotspots/112A7iiPUF6cKCRGE4CT646LkQJ5gUmU51HsFYqtW5914fN9pMub/activity?activityRange=8H&selectedActivities=Witness" | grep -o "\(Uns\|S\)elected"
-witnessing=$( grep -o '\('$key1'\)\|\('$key2'\)' "$cache_dir/$page" )
+witnessing=$( grep -o '\('$key1'\)\|\('$key2'\)' "$cachedir/$page" )
 #
 [ $DBG ] && echo "DBG.witnessing: "$witnessing && echo
 
